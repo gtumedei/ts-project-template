c1659cfa436466a54ccc0cb5eca47438ba8634fe
gtumedei <giannitumedei@gmail.com>
2023-01-27 10:56:49 +0100

    Add __filename and __dirname

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { sayHello, sayGoodbye } from "./utils"
+import { sayHello, sayGoodbye, getFilename, getDirname } from "./utils"
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`__filename: ${getFilename(import.meta.url)}}`)
+console.log(`__dirname: ${getDirname(import.meta.url)}`)
 
 sayHello()
 await new Promise(r => setTimeout(r, 100))
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,7 @@
+import { fileURLToPath } from "url"
+
 export const sayHello = () => console.log("Hello, World!")
 export const sayGoodbye = () => console.log("Goodbye, World!")
+
+export const getFilename = (fileUrl: string) => fileURLToPath(fileUrl)
+export const getDirname = (fileUrl: string) => fileURLToPath(new URL(".", fileUrl))
